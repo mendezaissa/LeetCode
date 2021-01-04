ab7b1fa88dee680351c289360a2777cd1773f287
Aissa Mendez <amendez@C02CM97UML7H.local>
2021-01-04 13:10:49 -0600

    709 To Lower Case

---
+++ src/To Lower Case.java
@@ -0,0 +1,11 @@
+//To Lower Case
+//8 / 8 test cases passed.
+//Status: Accepted
+//Runtime: 0 ms
+
+class Solution {
+    public String toLowerCase(String str) {
+        
+        return str.toLowerCase();
+    }
+}
